9d45e474358fb0af0f510d8deeb26eefa3a5fa65
Hao Feng <2410772045@qq.com>
2024-03-29 22:33:48 +0800

    init

---
+++ Common/PrePostProcessor.cpp
@@ -0,0 +1,3 @@
+#include "PrePostProcessor.h"
+
+
---
+++ Common/PrePostProcessor.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <openvino/openvino.hpp>
+
+#include <vector>
+
+
+
+namespace Common
+{
+
+    class PrePostProcessor
+    {
+    public:
+        PrePostProcessor() = default;
+        virtual ~PrePostProcessor() {};
+
+        virtual std:
+
+    };
+}
+
+
+
